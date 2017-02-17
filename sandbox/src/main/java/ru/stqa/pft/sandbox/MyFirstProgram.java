64b56d2d2ef1ecd9ba8aac4051f08e12b663a91c
Bughuntress <qazakhidat@gmail.com>
2017-02-17 11:11:26 +0300

    Создаем методы и функции

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -5,21 +5,16 @@ public class MyFirstProgram {
         hello("World!");
         hello("Baby");
         hello("Zakhidat Xanim!");
-        double l = 5;
-        System.out.println("Площадь квадрата со стороной "+l+" равна "+area(l));
-        double a =2;
-        double b = 2;
-        System.out.println("Площадь квадрата со стороной "+a+" и со стороной "+b+" равна "+area(a,b));
+        Square s = new Square(5);
+        System.out.println("Площадь квадрата со стороной "+s.l+" равна "+s.area());
+        Rectangle r = new Rectangle(2,2);
+        System.out.println("Площадь квадрата со стороной "+r.a+" и со стороной "+r.b+" равна "+r.area());
     }
 
     public static void hello(String smbd) {
 
         System.out.println("Hello, "+smbd);
     }
-    public static double area(double l) {
-        return l*l;
-    }
-    public static double area(double a, double b) {
-        return a*b;
-    }
+
+
 }
