dea944c3518b7338bf87b9d3d1f4737632b38d10
Bughuntress <qazakhidat@gmail.com>
2017-03-14 22:49:46 +0300

    4л: Формирование коллекции

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
@@ -4,24 +4,17 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.GroupData;
 
+import java.util.List;
 
-public class GroupDeletionTests extends TestBase {
-
+public class GroupCreationTests extends TestBase{
 
   @Test
-  public void testGroupDeletion() {
-
+  public void testGroupCreation() {
     app.getNavigationHelper().gotoGroupPage();
-    int before = app.getGroupHelper().getGroupCount();
-    if (!app.getGroupHelper().isThereAGroup()){
-      app.getGroupHelper().createGroup(new GroupData("Сказочные герои", "name", null));
-    }
-    app.getGroupHelper().selectGroup();
-    app.getGroupHelper().deleteSelectedGroups();
-    app.getGroupHelper().returnToGroupPage();
-    int after = app.getGroupHelper().getGroupCount();//кол-во групп после добавления
-    Assert.assertEquals(after, before-1);
+    List<GroupData> before = app.getGroupHelper().getGroupList();
+    app.getGroupHelper().createGroup(new GroupData("Сказочные герои", "name", null));
+    List<GroupData> after = app.getGroupHelper().getGroupList();
+    Assert.assertEquals(after.size(), before.size()+1);
   }
-
 
 }
